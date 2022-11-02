5eb1d2aae216e3b4de4e78d31bbf8bb52746fc0a
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-02 15:41:30 +0100

    Modifice alla select: - se non scegli una difficoltà non puoi proseguire con il gioco - creata una funzione per cui in base alla select-option scelta ti genera tot numeri

--- js/main.js
+++ js/main.js
@@ -1,28 +1,56 @@
 "use strict";
 
-// Consegna:
+/*---------------
+    FUNCTIONS
+---------------*/
+function checkSelectValue(select){
+    if (select.selectedIndex === 1){
+        return 49;
+    } else if (select.selectedIndex === 2){
+        return 81;
+    } else {
+        return 100;
+    }
+};
 
+/*---------------
+    MAIN
+---------------*/
 const cellContainer = document.querySelector(".cell-container");
 const playGameButton = document.getElementById("play-game");
+const selectBody = document.getElementById("select-body");
+selectBody.insane = 100;
 
 /*L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.*/
+Ogni cella ha un numero progressivo, da 1 a 100.
+Se non si sceglie la difficoltà, il gioco non parte.*/
+
 playGameButton.addEventListener("click", function(){
-    cellContainer.innerHTML = "";
-    for(let i=1; i<=100; i++){
-        const cellSquare = document.createElement("div");
-        cellSquare.classList.add("cell-item");
-        cellSquare.innerHTML = i;
-
-        /*Quando l'utente clicca su ogni cella, la cella cliccata si colora di 
-        azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
-        cellSquare.addEventListener("click", function(){
-            this.classList.add("blue-bg-color");
-            console.log(this.innerHTML);
-        });
-
-        cellContainer.append(cellSquare);
-    }
+    if(selectBody.value === "default"){
+        alert("Attenzione! Scegli la difficoltà!");
+    } else {
+        cellContainer.innerHTML = "";
+        for(let i=1; i<= checkSelectValue(selectBody); i++){
+            const cellSquare = document.createElement("div");
+            if(selectBody.value === "insane"){
+                cellSquare.classList.add("cell-item", "difficulty-insane");
+            } else if(selectBody.value === "hard"){
+                cellSquare.classList.add("cell-item", "difficulty-hard");
+            } else {
+                cellSquare.classList.add("cell-item", "difficulty-normal");
+            }
+            cellSquare.innerHTML = i;
+    
+            /*Quando l'utente clicca su ogni cella, la cella cliccata si colora di 
+            azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
+            cellSquare.addEventListener("click", function(){
+                this.classList.add("blue-bg-color");
+                console.log(this.innerHTML);
+            });
+    
+            cellContainer.append(cellSquare);
+        }
+    } 
 });
 
 
